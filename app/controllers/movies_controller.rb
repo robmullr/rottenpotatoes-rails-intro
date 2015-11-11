6bdd57cd94689e3174ee9421fed67847038ef232
robmullr <robmullr@gmail.com>
2015-11-11 03:01:46 +0000

    sort works

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,34 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+     sort = params[:sort] || session[:sort]
+     case sort
+     when 'title'
+       ordering,@title_header = {:order => :title}, 'hilite'
+     when 'release_date'
+       ordering,@date_header = {:order => :release_date}, 'hilite'
+     end
+     @all_ratings = Movie.all_ratings
+     @selected_ratings = params[:ratings] || session[:ratings] || {}
+     if @selected_ratings == {}
+       @selected_ratings = Hash[@all_ratings.map {|rating| [rating, rating]}]
+     end
+     if params[:sort] != session[:sort]
+       session[:sort] = sort
+       flash.keep
+       redirect_to :sort => sort, :ratings => @selected_ratings and return
+     end
+     if params[:ratings] != session[:ratings] and @selected_ratings != {}
+       session[:sort] = sort
+       session[:ratings] = @selected_ratings
+       flash.keep
+       redirect_to :sort => sort, :ratings => @selected_ratings and return
+     end
+     order = 'asc'
+     #@movies = Movie.where(:rating => @selected_ratings.keys).order(ordering :asc)
+     #@movies = Movie.find_all_by_rating(@selected_ratings.keys, ordering)
+     #@movies = Movie.where(:rating =>@selected_ratings.keys).order(ordering.to_sym :asc)
+     @movies = Movie.order("#{sort} #{order}").where('rating IN (?)', @selected_ratings.keys).all
   end
 
   def new
